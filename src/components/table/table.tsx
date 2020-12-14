848caa19b147f44a4abedf7a3b50b63b3d937571
Minh Duc <ductm@devblock.net>
2020-12-14 16:56:37 +0700

    Update Archive Page

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,8 +1,29 @@
 import * as React from 'react';
 import { Container } from './styles/table';
+import PropTypes from 'prop-types';
 
-const Table = ({ children, ...restProps }: React.HTMLAttributes<HTMLElement>) => {
-  return <Container {...restProps}>{children}</Container>;
+interface TableProps extends React.HTMLProps<HTMLElement> {
+  show?: boolean;
+}
+
+const defaultProps = {
+  show: true,
 };
+
+const propTypes = {
+  show: PropTypes.bool,
+};
+
+type Table = React.ForwardRefExoticComponent<TableProps> & {};
+
+const Table = React.forwardRef<HTMLDivElement, TableProps>((uncontrolledProps: TableProps, ref) => {
+  const { children } = uncontrolledProps;
+
+  return <Container ref={ref}>{children}</Container>;
+}) as Table;
+
+Table.displayName = 'Table';
+Table.defaultProps = defaultProps as any;
+Table.propTypes = propTypes;
 
 export default Table;
