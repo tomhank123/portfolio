f5bb0ad10d8e22cbcbc28ed0fe982fc99a8a61f9
Minh Duc <ductm@devblock.net>
2021-01-26 18:27:06 +0700

    Remove Technology component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,9 @@
 /* PLOP_COMPONENTS_EXPORT */
-export { default as Side } from './side';
 export { default as Biography } from './biography';
 export { default as Contact } from './contact';
 export { default as Table } from './table';
 export { default as Footer } from './footer';
 export { default as Jumbotron } from './jumbotron';
-export { default as Technology } from './technology';
 export { default as Project } from './project';
 export { default as Featured } from './featured';
 export { default as Jobs } from './jobs';
