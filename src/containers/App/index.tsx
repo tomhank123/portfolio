0be35a0e9ce5b32a0744a150c6adec0d99bb4118
Duc Tran Minh <ductm@devblock.net>
2021-01-27 21:32:23 +0700

    Remove route file

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,16 +1,18 @@
-import routes from '@/routes';
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ROUTES } from '@/utils/constants';
+
 import NotFoundPage from '@/containers/NotFoundPage';
+import ArchivePage from '@/containers/ArchivePage';
+import Homepage from '@/containers/HomePage';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        {routes.map((route) => (
-          <Route key={route.path} exact path={route.path} component={route.component} />
-        ))}
-        <Route component={NotFoundPage} />
+        <Route exact path={ROUTES.HOME} component={Homepage} />
+        <Route exact path={ROUTES.ARCHIVE} component={ArchivePage} />
+        <Route path='' component={NotFoundPage} />
       </Switch>
     </Router>
   );
