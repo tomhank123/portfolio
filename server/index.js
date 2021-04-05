eb568c8082c98c1f4fcedd7fda5af6951d237f3c
Duc Tran Minh <ductm@devblock.net>
2021-04-05 21:44:39 +0700

    feat: add static folder

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ const app = express();
 // app.use('/api', myApi);
 
 // In production we need to pass these values in instead of relying on webpack
+app.use(express.static(resolve(process.cwd(), 'static')));
 setup(app, {
   outputPath: resolve(process.cwd(), 'build'),
   publicPath: '/',
